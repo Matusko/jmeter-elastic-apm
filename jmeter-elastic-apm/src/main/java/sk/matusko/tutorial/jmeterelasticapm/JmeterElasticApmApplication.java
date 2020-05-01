0a19a756a787250f7942bac1170d9355037965a8
matus <bartko.mat@gmail.com>
2020-05-01 20:13:42 +0200

    spring boot project initialized

---
+++ jmeter-elastic-apm/src/main/java/sk/matusko/tutorial/jmeterelasticapm/JmeterElasticApmApplication.java
@@ -0,0 +1,13 @@
+package sk.matusko.tutorial.jmeterelasticapm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JmeterElasticApmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JmeterElasticApmApplication.class, args);
+	}
+
+}
---
+++ jmeter-elastic-apm/src/test/java/sk/matusko/tutorial/jmeterelasticapm/JmeterElasticApmApplicationTests.java
@@ -0,0 +1,13 @@
+package sk.matusko.tutorial.jmeterelasticapm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JmeterElasticApmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
